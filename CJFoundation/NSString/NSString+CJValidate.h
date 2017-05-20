435704040851718779cec30208d14dd5712c0a98
dvlproad <studyroad@qq.com>
2017-05-20 20:33:55 +0800

    【Optimization】对NSString增加数字和字母的判断cj_validate

--- CJFoundation/NSString/NSString+CJValidate.h
+++ CJFoundation/NSString/NSString+CJValidate.h
@@ -12,28 +12,42 @@
 
 //源自：iOS - 正则表达式判断邮箱、身份证..是否正确：http://www.2cto.com/kf/201311/256494.html
 
-//邮箱
+#pragma mark - 数字、字母、整型、浮点型的判断
+///判断是否为整型
+- (BOOL)cj_validateInt;
+//判断是否为浮点型
+- (BOOL)cj_validateFloat;
+
+///判断仅输入数字或字母
+- (BOOL)cj_validateNumberOrLetter;
+///判断是否纯字母
+- (BOOL)cj_validateAllLetter;
+///验证是否是纯数字
+- (BOOL)cj_validateAllNumber;
+
+#pragma mark - 其他判断
+///邮箱
 - (BOOL)cj_validateEmail;
 
-//手机号码验证
+///手机号码验证
 - (BOOL)cj_validateMobile;
 
-//车牌号验证
+///车牌号验证
 - (BOOL)cj_validateCarNo;
 
-//车型
+///车型
 - (BOOL)cj_validateCarType;
 
-//用户名
+///用户名
 - (BOOL)cj_validateUserName;
 
-//密码
+///密码
 - (BOOL)cj_validatePassword;
 
-//昵称
+///昵称
 - (BOOL)cj_validateNickname;
 
-//身份证号
+///身份证号
 - (BOOL)cj_validateIdentityCard;
 
 @end
--- CJFoundation/NSString/NSString+CJValidate.m
+++ CJFoundation/NSString/NSString+CJValidate.m
@@ -10,16 +10,79 @@
 
 @implementation NSString (CJValidate)
 
-//邮箱
-- (BOOL)cj_validateEmail{
+#pragma mark - 数字、字母、整型、浮点型的判断
+///判断是否为整型
+- (BOOL)cj_validateInt {
+    NSScanner* scan = [NSScanner scannerWithString:self];
+    int val;
+    return [scan scanInt:&val] && [scan isAtEnd];
+}
+
+//判断是否为浮点型
+- (BOOL)cj_validateFloat {
+    NSScanner* scan = [NSScanner scannerWithString:self];
+    float val;
+    return [scan scanFloat:&val] && [scan isAtEnd];
+}
+
+/*
+ //demo
+ if( ![insertValue.text cj_validateInt] || ![insertValue.text cj_validateFloat])
+ {
+ resultLabel.textColor = [UIColor redColor];
+ resultLabel.text = @"警告:含非法字符，请输入纯数字！";
+ return;
+ }
+ */
+
+///判断仅输入数字或字母
+- (BOOL)cj_validateNumberOrLetter {
+    if (self.length == 0) {
+        return NO;
+    }
+    
+    NSString *regex =@"[a-zA-Z0-9]*";
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",regex];
+    return [predicate evaluateWithObject:self];
+}
+
+///判断是否纯字母
+- (BOOL)cj_validateAllLetter {
+    if (self.length == 0) {
+        return NO;
+    }
+    
+    NSString *regex =@"[a-zA-Z]*";
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",regex];
+    return [predicate evaluateWithObject:self];
+}
+
+///验证是否是纯数字
+- (BOOL)cj_validateAllNumber {
+    if (self.length == 0) {
+        return NO;
+    }
+    
+    NSString *regex =@"[0-9]*";
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",regex];
+    return [predicate evaluateWithObject:self];
+}
+
+#pragma mark - 其他判断
+///邮箱
+- (BOOL)cj_validateEmail {
+    if (self.length == 0) {
+        return NO;
+    }
+    
     NSString *emailRegex = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
     NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegex];
     return [emailTest evaluateWithObject:self];
 }
 
 
-//手机号码验证
-- (BOOL)cj_validateMobile{
+///手机号码验证
+- (BOOL)cj_validateMobile {
     //手机号以13， 15，18开头，八个 \d 数字字符
     NSString *phoneRegex = @"^((13[0-9])|(15[^4,\\D])|(18[0,0-9]))\\d{8}$";
     NSPredicate *phoneTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",phoneRegex];
@@ -27,9 +90,8 @@
 }
 
 
-//车牌号验证
-- (BOOL)cj_validateCarNo
-{
+///车牌号验证
+- (BOOL)cj_validateCarNo {
     NSString *carRegex = @"^[\u4e00-\u9fa5]{1}[a-zA-Z]{1}[a-zA-Z_0-9]{4}[a-zA-Z_0-9_\u4e00-\u9fa5]$";
     NSPredicate *carTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",carRegex];
     NSLog(@"carTest is %@",carTest);
@@ -37,16 +99,16 @@
 }
 
 
-//车型
-- (BOOL)cj_validateCarType{
+///车型
+- (BOOL)cj_validateCarType {
     NSString *CarTypeRegex = @"^[\u4E00-\u9FFF]+$";
     NSPredicate *carTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",CarTypeRegex];
     return [carTest evaluateWithObject:self];
 }
 
 
-//用户名
-- (BOOL)cj_validateUserName{
+///用户名
+- (BOOL)cj_validateUserName {
     NSString *userNameRegex = @"^[A-Za-z0-9]{6,20}+$";
     NSPredicate *userNamePredicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",userNameRegex];
     BOOL B = [userNamePredicate evaluateWithObject:self];
@@ -54,24 +116,24 @@
 }
 
 
-//密码
-- (BOOL)cj_validatePassword{
+///密码
+- (BOOL)cj_validatePassword {
     NSString *passWordRegex = @"^[a-zA-Z0-9]{6,20}+$";
     NSPredicate *passWordPredicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",passWordRegex];
     return [passWordPredicate evaluateWithObject:self];
 }
 
 
-//昵称
-- (BOOL)cj_validateNickname{
+///昵称
+- (BOOL)cj_validateNickname {
     NSString *nicknameRegex = @"^[\u4e00-\u9fa5]{4,8}$";
     NSPredicate *passWordPredicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",nicknameRegex];
     return [passWordPredicate evaluateWithObject:self];
 }
 
 
-//身份证号
-- (BOOL)cj_validateIdentityCard{
+///身份证号
+- (BOOL)cj_validateIdentityCard {
     BOOL flag;
     if (self.length <= 0) {
         flag = NO;
