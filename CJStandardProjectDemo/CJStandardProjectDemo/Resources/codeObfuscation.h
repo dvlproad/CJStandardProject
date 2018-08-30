0a6d8c99db4dffc117f1c4662648eac134c25e69
dvlproad <studyroad@qq.com>
2018-08-30 18:22:14 +0800

    【Feature】代码混淆：增加类名混淆

--- CJStandardProjectDemo/CJStandardProjectDemo/Resources/codeObfuscation.h
+++ CJStandardProjectDemo/CJStandardProjectDemo/Resources/codeObfuscation.h
@@ -1,7 +1,12 @@
 #ifndef Demo_codeObfuscation_h
 #define Demo_codeObfuscation_h
-//confuse string at Thu Aug 30 15:01:30 CST 2018
-#define custom_method0 mQFQjyMzfwkjTiMT
-#define custom_method1 hSeshXcQDBfPrTKZ
-#define custom_method2 BFuwyWZnMztsIzsT
+//confuse string at Thu Aug 30 15:06:35 CST 2018
+#define custom_method0 woshisuibian
+#define custom_method1 woluanlai
+#define custom_method2 wobieqianwan
+#define bindViewModel  woshibangdingVM
+#define goRegisterViewController woyaoquzhuce
+#define s2 woshigecanshu
+#define LoginViewController woshijiami
+#define AppDelegate woshirukou
 #endif
